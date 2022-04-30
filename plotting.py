3bad92ee2562d0c17395e32662e260d6ac75cadf
RhysEshelman <69173060+RhysEshelman@users.noreply.github.com>
2022-04-30 13:28:54 -0500

    Add files via upload

--- plotting.py
+++ plotting.py
@@ -1,9 +1,20 @@
 import pickle as pkl
 import matplotlib.pyplot as plt
 
-dataList = pkl.load(open('GeneticsData.pkl', 'rb'))
-plt.plot(dataList[0], dataList[1])
-plt.title('Average Scores over Each Generation')
-plt.xlabel('Generation')
+dataList = pkl.load(open('GeneticsDefault.pkl', 'rb'))
+fig, axs = plt.subplots(2,sharex=True)
+fig.suptitle('Average Winrates and Scores over each Generation')
+axs[0].plot(dataList[0],dataList[1])
+axs[1].plot(dataList[0],dataList[2])
+axs[1].set(xlabel='Generation',ylabel='Average Winrate')
+axs[0].set(ylabel='Average Score')
+plt.savefig('GeneticsDefault.png')
+
+"""dataList = pkl.load(open('ApproxQLearning.pkl', 'rb'))
+plt.plot(dataList[0],dataList[1],label='Windowed Average')
+plt.plot(dataList[0],dataList[2],label='Total Average')
+plt.xlabel('Training Episodes Complete')
 plt.ylabel('Average Score')
-plt.savefig('GeneticScores.png')
+plt.title('Average Score over Training for Approximate Q-Learning')
+plt.legend()
+plt.savefig('ApproxQLearning.png')"""
